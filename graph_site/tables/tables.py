bba05f90b7420cb32d7abf6db80a2f5d51599a94
GovardFilipsovich <ricardrajonhart@gmail.com>
2023-05-17 14:20:15 +0300

    Added logging of results

--- graph_site/tables/tables.py
+++ graph_site/tables/tables.py
@@ -1,13 +1,15 @@
 import django_tables2 as tables
 from ..services.math_services import get_nodes
-
+from django_tables2.export.export import TableExport
+import json
+import datetime
 
 class GraphTable(tables.Table):
     def __init__(self, graph:list[int]):
-        print(graph)
         data = []
         # Получаем все узлы графа
         nodes = get_nodes(graph)
+        print(nodes)
         # Получаем кол-во узлов, чтобы не получать много раз в цикле
         nodes_count = len(nodes)
         self.base_columns["#"] = tables.Column(attrs={"th":{"class":"table-light text-center"}})
@@ -21,8 +23,23 @@ class GraphTable(tables.Table):
                 else:
                     row[j] = 0
             data.append(row)
+        print([i for i in self.base_columns])
+        cur_nodes = []
+        for i in list(self.base_columns.keys())[1:]:
+            if int(i) not in nodes:
+                cur_nodes.append((i, None))
+        super().__init__(data, extra_columns=cur_nodes)
+        print([i for i in self.base_columns])
+        # Экспортирование новосозданной таблички
+        export = TableExport('csv', self)
+        try:
+            json_log = json.load(open("graph_site/logfiles/log.json", 'r'))
+        except json.JSONDecodeError:
+            json.dump([{"date":datetime.datetime.now().strftime("%d.%m.%Y"), "content":export.export()}], open("graph_site/logfiles/log.json", "w"))
+        else:
+            json_log.append({"date":datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"), "content":export.export()})
+            json.dump(json_log, open("graph_site/logfiles/log.json", "w"))
 
-        super().__init__(data)
 
     class Meta:
         attrs = {"class":"table table-bordered",

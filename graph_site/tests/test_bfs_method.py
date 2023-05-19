a212cd0a2a920498b3d726fa7675e521ff7127bb
GovardFilipsovich <ricardrajonhart@gmail.com>
2023-05-19 14:38:21 +0300

    Added test case

--- graph_site/tests/test_bfs_method.py
+++ graph_site/tests/test_bfs_method.py
@@ -1,4 +1,6 @@
 from unittest import TestCase
+from graph_site.services.math_services import bfs_algorithm
+
 
 
 class Test(TestCase):
@@ -10,5 +12,6 @@ class Test(TestCase):
             [1, [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)], [(1, 2), (1, 3), (1, 4)]],
             [1, [(1, 2), (1, 3), (2, 3), (4, 5), (4, 6), (5, 6)], [(1, 2), (1, 3)]]
         ]
-        
+        for i in graphs:
+            self.assertEqual(bfs_algorithm(i[1], i[0]), i[2])
     

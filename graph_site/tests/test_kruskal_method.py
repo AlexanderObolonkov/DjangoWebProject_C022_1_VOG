98d2c7793b0cf4a9d8be7ed22777a679422e3bdb
GovardFilipsovich <ricardrajonhart@gmail.com>
2023-05-19 17:24:54 +0300

    Refactor tests

--- graph_site/tests/test_kruskal_method.py
+++ graph_site/tests/test_kruskal_method.py
@@ -4,6 +4,7 @@ from graph_site.services.math_services import kruskal_algorithm
 
 class Test(TestCase):
     def test_match_dfs_result(self):
+        #arrange
         graphs = [
             [[(1, 3, 1), (1, 2, 3), (1, 4, 5), (2, 3, 3), (2, 4, 4), (2, 5, 7), (3, 5, 8), (4, 5, 10)],
              [(1, 3, 1), (1, 2, 3), (2, 4, 4), (2, 5, 7)]],
@@ -14,5 +15,14 @@ class Test(TestCase):
             [[(1, 2, 1), (1, 3, 1), (2, 3, 1), (4, 5, 1), (4, 6, 1), (5, 6, 2)],
              [(1, 2, 1), (1, 3, 1), (4, 5, 1), (4, 6, 1)]],
         ]
-        for i in graphs:
-            self.assertEqual(kruskal_algorithm(i[0]), i[1])
+        #act
+        results = [
+            [(1, 3, 1), (1, 2, 3), (2, 4, 4), (2, 5, 7)],
+            [(1, 2, 2), (1, 3, 2), (1, 4, 2), (4, 5, 2), (4, 6, 2)],
+            [(1, 2, 1), (1, 3, 3)],
+            [(1, 4, 1), (2, 3, 3), (2, 4, 3)],
+            [(1, 2, 1), (1, 3, 1), (4, 5, 1), (4, 6, 1)]
+                ]
+        #assert
+        for i in range(len(graphs)):
+            self.assertEqual(kruskal_algorithm(graphs[i]), results[i])

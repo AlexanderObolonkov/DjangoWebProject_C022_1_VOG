37207cf9a4c461844d634609fa18ac5da01dcbc0
aleksandr obolonkov <itbolonkov2019@gmail.com>
2023-05-19 22:41:41 +0300

    Tests refactoring

--- graph_site/tests/test_bfs_method.py
+++ graph_site/tests/test_bfs_method.py
@@ -2,9 +2,9 @@ from django.test import TestCase
 from graph_site.services.math_services import bfs_algorithm
 
 
-class Test(TestCase):
+class TestBFSMethod(TestCase):
     def test_match_bfs_result(self):
-        #arrange
+        # arrange
         graphs = [
             [1, [(1, 3), (1, 2), (1, 4), (2, 3), (2, 4), (2, 5), (3, 5), (4, 5)]],
             [1, [(1, 2), (1, 3), (1, 4), (4, 5), (4, 6), (5, 6)]],
@@ -12,14 +12,14 @@ class Test(TestCase):
             [1, [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]],
             [1, [(1, 2), (1, 3), (2, 3), (4, 5), (4, 6), (5, 6)]]
         ]
-        #act
-        results = [
+        # expected
+        expected_results = [
             [(1, 3), (1, 2), (1, 4), (3, 5)],
             [(1, 2), (1, 3), (1, 4), (4, 5), (4, 6)],
             [(1, 2), (1, 3)],
             [(1, 2), (1, 3), (1, 4)],
             [(1, 2), (1, 3)]
                 ]
-        #assert
+        # assert
         for ind, case_ in enumerate(graphs):
-            self.assertEqual(bfs_algorithm(case_[1], case_[0]), results[ind])
+            self.assertEqual(bfs_algorithm(case_[1], case_[0]), expected_results[ind])
--- graph_site/tests/test_dfs_method.py
+++ graph_site/tests/test_dfs_method.py
@@ -2,9 +2,9 @@ from django.test import TestCase
 from graph_site.services.math_services import dfs_algorithm
 
 
-class Test(TestCase):
+class TestDFSMethod(TestCase):
     def test_match_dfs_result(self):
-        #arrange
+        # arrange
         graphs = [
             [1, [(1, 3), (1, 2), (1, 4), (2, 3), (2, 4), (2, 5), (3, 5), (4, 5)]],
             [1, [(1, 2), (1, 3), (1, 4), (4, 5), (4, 6), (5, 6)]],
@@ -12,7 +12,7 @@ class Test(TestCase):
             [1, [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]],
             [1, [(1, 2), (1, 3), (2, 3), (4, 5), (4, 6), (5, 6)]],
         ]
-        #act
+        # act
         results = [
             [(1, 3), (3, 2), (2, 4), (4, 5)],
             [(1, 2), (1, 3), (1, 4), (4, 5), (5, 6)],
@@ -20,6 +20,6 @@ class Test(TestCase):
             [(1, 2), (2, 3), (3, 4)],
             [(1, 2), (2, 3)]
                 ]
-        #assert
-        for ind, case_ in graphs:
+        # assert
+        for ind, case_ in enumerate(graphs):
             self.assertEqual(dfs_algorithm(case_[1], case_[0]), results[ind])
